2a85889fe9210ef88554124bd15f70ca1cec9146
justlive1 <qq11419041@163.com>
2018-03-29 12:30:23 +0800

    move from earth

---
+++ mist-api/src/main/java/net/oschina/git/justlive1/breeze/mist/api/model/User.java
@@ -0,0 +1,32 @@
+package net.oschina.git.justlive1.breeze.mist.api.model;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 用户
+ *
+ * @author wubo
+ */
+@Data
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private String id;
+  /**
+   * 用户名
+   */
+  private String name;
+
+  /**
+   * 密码
+   */
+  private String password;
+
+  /**
+   * 是否启用
+   */
+  private Integer enabled;
+
+}
